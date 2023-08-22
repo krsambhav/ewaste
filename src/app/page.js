5bac1179d7fd012d7ec96b6f885605bb992ed260
Kumar Sambhav <krsambhav16@gmail.com>
2023-08-22 20:38:09 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -184,7 +184,7 @@ export default function Home() {
                   name="email"
                   id="email"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  placeholder="1/2/3 kgs"
+                  placeholder="Location"
                   value={location}
                   onChange={(e) => setLocation(e.target.value)}
                 />
