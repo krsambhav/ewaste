4cc784c7255cbe0dbf3ad3816927421a96128716
Kumar Sambhav <krsambhav16@gmail.com>
2023-08-23 09:33:45 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -210,7 +210,7 @@ export default function Home() {
                   name="email"
                   id="email"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  placeholder="1/2/3 kgs"
+                  placeholder="Seller Name"
                   value={seller}
                   onChange={(e) => setSeller(e.target.value)}
                 />
@@ -221,7 +221,7 @@ export default function Home() {
                 htmlFor="email"
                 className="mt-5 block text-sm font-medium leading-6 text-gray-900"
               >
-                Location
+                Contact
               </label>
               <div className="mt-2">
                 <input
